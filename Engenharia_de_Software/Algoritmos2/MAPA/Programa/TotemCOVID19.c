2ed3797a65e29173d3fd8c4dfc930179444df57e
Andrei Lino <eng.andreilino@gmail.com>
2021-06-29 09:26:40 -0300

    PROJETO INICIAL DEV++
    Inicio do projeto aqui.

---
+++ Engenharia_de_Software/Algoritmos2/MAPA/Programa/TotemCOVID19.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+
+/*	-------------------------	FUNÇÃO PRINCIPAL ----------------------------	*/
+int main(int argc, char *argv[]) {
+
+printf("teste inicial");
+
+
+
+
+/*	-------------------------	FIM DO MAIN ----------------------------	*/
+	system("pause");
+	return 0;
+}
