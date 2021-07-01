705e51e6da899b6eda7a81666690fa6a613a1058
Andrei Lino <eng.andreilino@gmail.com>
2021-06-30 22:39:15 -0300

    Criado codigos base para manipulação de arquivos texto. Usando metodo padrão CRUD.

--- Engenharia_de_Software/Algoritmos2/SalvaEmArquivoTXT/main.c
+++ Engenharia_de_Software/Algoritmos2/SalvaEmArquivoTXT/main.c
@@ -1,6 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 
-int main() {
-    printf("Hello, World!\n");
+/*  ---------------------------------- ESTA FUNÇÃO ABRE E FECHA UM ARQUIVO  ----------------------------------------  */
+int abreArquivo()
+{
+    // criando a variável ponteiro para o arquivo
+	FILE *pont_arq;
+
+	//abrindo o arquivo
+	pont_arq = fopen("arquivo.txt", "a");
+
+	// fechando arquivo
+	fclose(pont_arq);
+
+	if (pont_arq == NULL)
+{
+    printf("ERRO! O arquivo não foi aberto!\n");
+}
+else
+   {
+     printf("O arquivo foi aberto com sucesso!");
+   }
+
+	getch();
+	//system("pause");
+	return(0);
+}
+
+/*  ---------------------------------- ESTA FUNÇÃO ABRE E GRAVA UMA PALAVRA NO ARQUIVO  ----------------------------  */
+int abreEgrava()
+{
+
+	FILE *pont_arq; // cria variável ponteiro para o arquivo
+	char palavra[20]; // variável do tipo string
+
+	//abrindo o arquivo com tipo de abertura w -> apaga tudo dentro do arquivo e reescreve
+	//abrindo o arquivo com tipo de abertura a  -> Escreve na frente do arquivo
+
+	pont_arq = fopen("arquivo_palavra.txt", "a");
+
+	//testando se o arquivo foi realmente criado
+	if(pont_arq == NULL)
+	{
+	printf("Erro na abertura do arquivo!");
+	return 1;
+	}
+
+	printf("Escreva uma palavra para testar gravacao de arquivo: ");
+	scanf("%s", palavra);
+
+	//usando fprintf para armazenar a string no arquivo
+	fprintf(pont_arq, "%s\n", palavra);
+
+	//usando fclose para fechar o arquivo
+	fclose(pont_arq);
+
+	printf("Dados gravados com sucesso!");
+
+	getch();
+	return 0;
+}
+
+int main(void)
+{
+    abreArquivo();  // Cria ou se já existe abre e fecha arquivo
+    abreEgrava();   // Cria ou se já existe grava palavra em arquivo
+
+/* ---------------------------------------   BANCADA DE TESTE    --------------------------------------------------   */
+
+
+
+/* ---------------------------------------------    FIM     -------------------------------------------------------   */
     return 0;
 }
