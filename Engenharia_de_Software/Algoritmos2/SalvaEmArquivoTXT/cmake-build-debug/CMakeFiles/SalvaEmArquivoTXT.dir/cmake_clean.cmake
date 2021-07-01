8267f38d948775c94e2293a74ecf90d6c7450b4f
Andrei Lino <eng.andreilino@gmail.com>
2021-06-30 22:05:05 -0300

    Manipulação de Arquivos TXT
    Inicio dos estudos de manipulação de arquivo em txt.

---
+++ Engenharia_de_Software/Algoritmos2/SalvaEmArquivoTXT/cmake-build-debug/CMakeFiles/SalvaEmArquivoTXT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SalvaEmArquivoTXT.dir/main.c.obj"
+  "SalvaEmArquivoTXT.exe"
+  "SalvaEmArquivoTXT.exe.manifest"
+  "SalvaEmArquivoTXT.pdb"
+  "libSalvaEmArquivoTXT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SalvaEmArquivoTXT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Engenharia_de_Software/Algoritmos2/SalvaEmArquivoTXT/cmake-build-debug/CMakeFiles/SalvaEmArquivoTXT.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SalvaEmArquivoTXT.
+# This may be replaced when dependencies are built.
