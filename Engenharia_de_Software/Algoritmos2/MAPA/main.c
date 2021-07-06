53ed475938554f30655b45b3214d2d68256e93ed
ANDREI DE ANDRADE <eng.andreilino@gmail.com>
2021-07-05 22:12:28 -0300

    Encaminha paciente para a ALA de acordo com sintomas

--- Engenharia_de_Software/Algoritmos2/MAPA/main.c
+++ Engenharia_de_Software/Algoritmos2/MAPA/main.c
@@ -165,67 +165,34 @@ int main()
     {
         cadastro(cad);
     }
-/*     -------------------------------------------------------------------------------------------------------------- */
+/*     ------------------------------------ Triagem para decisão de encaminhamentos --------------------------------- */
     linha();
-
     for (tri = 0; tri < 11; ++tri)
     {
         linha();
         triagem(tri);
     }
-//                                      TESTE DE MESA
-/*     ----------------------------------------------------------------------------------------------------- */
-
-    
-
-
-    /*     ----------------------------------------------------------------------------------------------------- */
+/*     ------------------------ Encaminha paciente para a ALA de acordo com sintomas  -------------------------------- */
+if(soma > 19){
+    printf("\nALTO RISCO\n");
+}
+else if(soma > 9){
+    printf("\nMÉDIO RISCO\n");
+}
+else
+{
+    printf("\nBAIXO RISCO\n");
+}
+/*     -------------------------------------------------------------------------------------------------------------- */
     getchar();
 //    system("pause");   ( APENAS NO WINDOWS )                  // MANTEM SISTEMA ABERTO ATÉ PRECIONAR QUALQUER TECLA
     return 0;
-}
-/*     ----------------------------------------------------------------------------------------------------- */
-
-/*     ----------------------------------------------------------------------------------------------------- */
-/*      Triagem para decidir para onde o paciente será direcionado
- *
- * Após o cadastro o paciente deve responder a um questionário de sintomas no qual cada sintoma terá um
-peso para avaliação do risco de COVID, os sintomas são:
-1 Tem Febre? (5 pontos)
-2 Tem dor de cabeça? (1 ponto)
-3 Tem secreção nasal ou espirros? (1 ponto)
-- Tem dor/irritação na garganta? (1 ponto)
-- Tem tosse seca? (3 pontos)
-- Tem dificuldade respiratória? (10 pontos)
-- Tem dores no corpo? (1 ponto)
-- Tem diarréia? (1 ponto)
-- Esteve em contato, nos últimos 14 dias, com um caso diagnosticado com COVID-19? (10 pontos)
-- Esteve em locais com grande aglomeração? (3 pontos)
-
-OBS: Caso o usuário indique que SIM (S) soma o valor, caso seja indicado NÃO (N) não soma o valor.
-
- */
-/*     ----------------------------------------------------------------------------------------------------- */
+}   // fim do MAIN
 
 /*
- O encaminhamento será feito para 3 alas, as de Risco Baixo, Risco Médio e Risco Alto.
-Para o risco baixo o total de pontos dos sintomas deverão ser de 0 a 9;
-Para o risco médio de 10 a 19 pontos;
- para o risco alto de 20 ou mais pontos.
+    Para finalizar deve ser salvo em um arquivo com os dados do paciente e qual sua pontuação de sintomas
+    para que seja realizada possíveis auditorias por parte da direção do hospital.
 
-Após preenchimento indicar para qual ala o paciente deverá se dirigir
- * */
-
-/*     ----------------------------------------------------------------------------------------------------- */
-
-/*
- Para finalizar deve ser salvo em um arquivo com os dados do paciente e qual sua pontuação de sintomas
-para que seja realizada possíveis auditorias por parte da direção do hospital.
-
- * */
-
-//system(“pause”) – chamada de sistema que “pausa” o programa e o faz aguardar pelo usuário;
-//* system(“cls”) – chamada de sistema que limpa os caracteres que foram impressos na tela anteriormente;
-//* fflush(stdin) – – limpar o buffer do teclado. Essa função deve sempre ser utilizada após ter utilizado alguma
-//função de entrada de dados como o scanf, gets, etc. Isso se dá pois em algumas situações o buffer do
-//teclado mantém “lixo de memória”, prejudicando a próxima operação de entrada do programa;
+    função de entrada de dados como o scanf, gets, etc. Isso se dá pois em algumas situações o buffer do
+    teclado mantém “lixo de memória”, prejudicando a próxima operação de entrada do programa;
+  * */
